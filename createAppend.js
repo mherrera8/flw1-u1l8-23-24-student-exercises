f1694ae9ab8571ce09e137674b05a281fc92fb85
mherrera8 <147006626+mherrera8@users.noreply.github.com>
2023-12-12 00:22:51 +0000

    Learned how to use .createElement and .appendChild

--- createAppend.js
+++ createAppend.js
@@ -4,30 +4,32 @@ let container = document.querySelector(".container");
 
 // 1. Create an h2 element.
 // - Store it in a variable called h2.
-
+let h2 = document.createElement("h2");
 
 // 2. Set the innerHTML of h2 to your name.
-
+h2.innerHTML = "Melania";
 
 
 // 3. Select the container div and append the h2 element.
 
-
+container.appendChild(h2);
 
 // 4. Create a paragraph element.
 // - Store it in a variable called para.
-
+let para = document.createElement("p");
 
 // 5. Set the innerHTML of para to a fact about yourself.
-
+para.innerHTML = "Has brown eyes.";
 
 // 6. Select the container div and append the paragraph element.
-
+container.appendChild(para);
 
 
 // Uncomment the code below after completing 1-6.
 // Where do you see this on the page?
+// Bellow the para element. 
 // Why is it located there even though the body is selected?
-// let h1 = document.createElement('h1');
-// h1.innerHTML = "My Webpage";
-// body.appendChild(h1);
+// This is here becuase of the order that the elements are in
+let h1 = document.createElement('h1');
+h1.innerHTML = "My Webpage";
+body.appendChild(h1);
--- jsLab.js
+++ jsLab.js
@@ -17,25 +17,30 @@ let tile9 = document.querySelector('.tile-9')
 // - Update the inner HTML to show an encouraging message! 
 // - Something like "YOU GOT THIS!" or even a "You're a beautiful person!" 
 
-
+tile1.innerHTML = "YOU GOT THIS!";
 
 // 2. Tile 2 has a button. 
 // - Create a click event and have the background color of tile 2 change to a shade of green.
 // - BE SURE TO SAVE THE BUTTON FIRST AS A VARIABLE.
 // - Can you see if there's a way to toggle it???
-
+let tile2Button = document.querySelector(".tile-2-button");
+tile2Button.addEventListener("click", function(){
+  tile2.style.backgroundColor = "green";
+});
 
 // 3. What's this? A totoro? Where's his friends? 
 // - Using createElement, create a friend (an image element).
 // - Once created, set the src of it to the image address.
 // - Append the element to tile 3. 
 // - Need some inspiration? Go to giphy.com, search a character and choose the "stickers".
-
+let friend = document.createElement("img");
+friend.src = "https://media4.giphy.com/media/39GAXpLVKvYRO/200w.webp?cid=ecf05e476dndye8h7x6ra4kvuwpgw5tuxnysu2v5iyzclgx0&ep=v1_gifs_search&rid=200w.webp&ct=g";
+tile3.appendChild(friend);
 
 // 4. Oh no! A broken image! We need to fix this.
 // - Set the src for tile 4 to have an image of your favorite TV show.
 
-
+tile4.src = "https://i.insider.com/650b0f2212dc4f001a1747b7?width=1000&format=jpeg&auto=webp";
 
 
 // These following tasks may require a little bit of digging. 
@@ -46,17 +51,20 @@ let tile9 = document.querySelector('.tile-9')
 
 // 5. Hmm. Looks like the font is too small. 
 // - Update the font-size of tile 5 to 35 pixels.
-
+tile5.style.fontSize = "35px";
 
 // 6. Another sad box! 
 // - Let make this tile 6 feel special and add a gradient background! 
 // - What kind of gradient? What color? YOU CHOOSE!
+tile6.style.background = "linear-gradient(to left, blue, red)";
 
 // 7. Too many squares. 
 // - Let's change tile 7 to have rounded edges - be sure to search for which CSS property does this :)
+tile7.style.borderRadius = "100px";
 
 // 8. Replace the image with your favorite emoji. 
 // - Try going to this site to search: https://emojipedia.org/
+
 
 // 9. Display a random number in the tile when the button is clicked.
 // - BE SURE TO SAVE THE BUTTON FIRST AS A VARIABLE.
